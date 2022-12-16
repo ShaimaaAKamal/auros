b5cc673dedbf87d5be9e55084398a1002f2d96ab
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-16 20:49:58 +0200

    make lazy loading for auth moudle in app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { BlogComponent } from './blog/blog.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   {path:'' , component:HomeComponent,title:'Home'},
   {path:'contact',component:ContactComponent,title:'Contact'},
-  {path:'blog',component:BlogComponent,title:'Blog'}
+  {path:'blog',component:BlogComponent,title:'Blog'},
+  {path:'auth',loadChildren:()=>import('./auth/auth.module').then((ms)=>ms.AuthModule)},
+  {path:'**' ,component:NotfoundComponent}
 ];
 
 @NgModule({
