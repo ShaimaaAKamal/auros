c859f9a633a20bb5ea8227ab2a0789fe64260787
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-16 23:56:31 +0200

    add shop module routing

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
+import { CompareComponent } from './compare/compare.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ShopComponent,title:'Shop'},
+  {path:'product/:id',component:ProductComponent,title:'Product'},
+  {path:'compare',component:CompareComponent,title:'Compare Products'},
+  {path:'checkout',component:CheckoutComponent,title:'Checkout'},
+  {path:'wishlist',component:WishlistComponent,title:'Wishlist'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
