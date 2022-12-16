d0c5e2b5016488b5b527f15fbcefb96165b4869b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-16 20:08:00 +0200

    add navbar to app componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProfileComponent } from './auth/profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    ShopComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
