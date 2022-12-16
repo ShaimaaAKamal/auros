079dc1b20d4421380b1b33b6e76e4d06ee239871
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-16 23:42:16 +0200

    make html component has dynamic data

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { User } from '../Interfaces/user';
+import { Product } from '../Interfaces/product';
+import { ShopInterface } from '../Interfaces/shop-interface';
 
-interface shopInterface{
-  title:string,
-  categories:string[]
-}
-
-interface Product{
-  prodImage:string,
-  name:string,
-  old:string,
-  new:string,
-
-}
 
 @Component({
   selector: 'app-navbar',
@@ -20,10 +11,16 @@ interface Product{
 })
 
 export class NavbarComponent implements OnInit{
-@ViewChild ('navContainer') navContainer:ElementRef;
 @ViewChild('collapse') collapse:ElementRef;
 
-shop:shopInterface[]=[{title:'Kitchen',categories:['Dinner Table','Dinning Chairs','Side Board','Cermaic Tiles','Seat']},
+
+closeSearch:boolean=false;
+cartItems:number=0;
+wishlistNumber:number=0;
+compareNumber:number=0;
+loggedUser:boolean=true;
+
+shop:ShopInterface[]=[{title:'Kitchen',categories:['Dinner Table','Dinning Chairs','Side Board','Cermaic Tiles','Seat']},
             {title:'Living Room',categories:['Sofa','TV Shelfs','Coffee Tables','Bed Sheet','Cusions']},
             {title:'Decoration',categories:['Lamps','Lighting','Furnitures','Speakers','Stools']}
            ]
@@ -31,8 +28,7 @@ productTitles:string[]=['Product Name','Product Feature','Product Special']
 products:Product[]=[{prodImage:'../../assets/images/ProductName/hummingbird-printed-t-shirt.jpg',name:'Teatop',old:"$43.90",new:'$21.90'},
                {prodImage:'../../assets/images/ProductName/brown-bear-printed-sweater.jpg',name:'Miro Dinning Table',old:"$83.90",new:'$52.90'},
                     {prodImage:'../../assets/images/ProductName/the-best-is-yet-to-come-framed-poster.jpg',name:'Janus Table Lamp',old:"$73.90",new:'$41.90'}]
-
-closeSearch:boolean=false;
+user:User={name:'ahmed'}
              
  closeMenu(){
   this.collapse.nativeElement.classList.remove('show');
