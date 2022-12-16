7ace7ccc7bf82f2a61e00bbeee8f2b12ad8f8fb1
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-16 20:45:01 +0200

    add basic routing and router Links in Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component:HomeComponent,title:'Home'},
+  {path:'contact',component:ContactComponent,title:'Contact'},
+  {path:'blog',component:BlogComponent,title:'Blog'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
