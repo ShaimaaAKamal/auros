9001003948f64c6b8c4b7acb61406735231c5c31
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-16 20:49:37 +0200

    add auth module routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ProfileComponent,title:'Profile'},
+  {path:'login',component:LoginComponent,title:'Login'},
+  {path:'register',component:RegisterComponent,title:'Register'},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
