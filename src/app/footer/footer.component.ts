3551a0f10a8be9b983d8414857830a35e45893dd
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-17 16:24:46 +0200

    implement footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+@ViewChild('productsMenu') productsMenu:ElementRef;
+@ViewChild('LinksMenu') LinksMenu:ElementRef;
 
+valid:boolean=true;
+emailAddress:string='';
+handleOpenClick(menuElement:HTMLElement){
+  menuElement.classList.toggle('static');
+}
+handleSubscribe(){
+  const regex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  this.valid=regex.test(this.emailAddress);
+}
 }
