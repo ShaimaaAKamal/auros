7b853f11f31e02459a2f4c08125ce0fd8f04dbd4
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-17 13:59:58 +0200

    add routes to about and  faqs component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { BlogComponent } from './blog/blog.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { FaqComponent } from './faq/faq.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {path:'' , component:HomeComponent,title:'Home'},
   {path:'contact',component:ContactComponent,title:'Contact'},
   {path:'blog',component:BlogComponent,title:'Blog'},
+  {path:'FAQS',component:FaqComponent,title:'FAQS'},
+  {path:'about',component:AboutComponent,title:'About'},
   {path:'auth',loadChildren:()=>import('./auth/auth.module').then((ms)=>ms.AuthModule)},
   {path:'shop',loadChildren:()=>import('./shop/shop.module').then((ms=>ms.ShopModule))},
   {path:'**' ,component:NotfoundComponent}
