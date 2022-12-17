1525f9f19be22bcdf44c6e4947e0e4263309365b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-17 12:03:44 +0200

    add categories route to shop route

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -5,11 +5,14 @@ import { ProductComponent } from './product/product.component';
 import { CompareComponent } from './compare/compare.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   {path:'',component:ShopComponent,title:'Shop'},
   {path:'product' ,redirectTo:'product/1'},
   {path:'product/:id',component:ProductComponent,title:'Product'},
+  {path:'categories', redirectTo:''},
+  {path:'categories/:id/:category',component:ProductsComponent,title:'Category'},
   {path:'compare',component:CompareComponent,title:'Compare Products'},
   {path:'checkout',component:CheckoutComponent,title:'Checkout'},
   {path:'wishlist',component:WishlistComponent,title:'Wishlist'}
