d04178a68eea94ac24f5ad7231ef0e5bcaf5b66f
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-18 12:44:03 +0200

    change component name

--- src/app/home/new-design/new-design.component.ts
+++ src/app/home/new-design/new-design.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-new-design',
+  selector: 'app-home-new-design',
   templateUrl: './new-design.component.html',
   styleUrls: ['./new-design.component.scss']
 })
