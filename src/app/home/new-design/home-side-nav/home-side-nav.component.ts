e21e5e46179a3ad8b177a21492d01455a9208701
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-18 23:42:00 +0200

    implment new design section

--- src/app/home/new-design/home-side-nav/home-side-nav.component.ts
+++ src/app/home/new-design/home-side-nav/home-side-nav.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as e from 'express';
 
 @Component({
   selector: 'app-home-side-nav',
   templateUrl: './home-side-nav.component.html',
   styleUrls: ['./home-side-nav.component.scss']
 })
-export class HomeSideNavComponent {
+export class HomeSideNavComponent implements OnInit {
+categories:boolean[]=[true,false,false,false,false]
+ Data:any[]=[];
+ ALLData=[
+     {
+       id: 1,
+       src:"../../../assets/images/trending/teapot/hummingbird-printed-t-shirt.jpg",
+       srcHover:"../../../assets/images/trending/teapot/hummingbird-printed-t-shirt3.jpg",
+       alt:'Side 1',
+       title:'Teapot',
+       productId:1,
+       old:"$43.90",new:'$21.90'
+     },
+     {
+       id: 2,
+       src:"../../../assets/images/trending/table/brown-bear-printed-sweater2.jpg",
+       srcHover:"../../../assets/images/trending/table/brown-bear-printed-sweater3.jpg",
+       alt:'Side 2',
+       title:'Miro Dinning Table',
+      productId:2,old:"$83.90",new:'$52.90',
+     },
+     {
+       id: 3,
+       src:"../../../assets/images/trending/TableLamp/the-best-is-yet-to-come-framed-poster2.jpg",
+       srcHover:"../../../assets/images/trending/TableLamp/the-best-is-yet-to-come-framed-poster.jpg",
+       alt:'Side 3',
+       title:'Janus Table Lamp',
+       productId:3,
+       old:"$73.90",new:'$41.90'
+     },
+     {
+       id: 4,
+       src:"../../../assets/images/trending/Discus/the-adventure-begins-framed-poster.jpg",
+       srcHover:"../../../assets/images/trending/Discus/the-adventure-begins-framed-poster2.jpg",
+       alt:'Side 4',
+       title:'Discus Floor and Table',
+       productId:4,
+        old:"$199.90",new:'$150.90'
+     },
+   ]
+
+  getData(category:string){
+     switch(category){
+      case 'all': this.Data=this.ALLData; this.categories=this.categories.map((category,index)=> index === 0);break;
+      case 'Furnitures': this.Data=this.ALLData.slice(0,2);this.categories=this.categories.map((category,index)=> index === 1);break;
+      case 'Chairs': this.Data=this.ALLData.slice(0,3); this.categories=this.categories.map((category,index)=> index === 2); break;
+      case 'Lighting': this.Data=this.ALLData.slice(0,2); this.categories=this.categories.map((category,index)=> index === 3); break;
+      case 'Decor': this.Data=[]; this.categories=this.categories.map((category,index)=> index === 4); break;
+     }
+  }
+   ngOnInit(): void {
+       this.Data=this.ALLData;
+   }
+   handleFocus(category:string){
+       this.getData(category);
+   }
 
 }
--- src/app/home/new-design/home-side-nav/sidenav-items/sidenav-items.component.ts
+++ src/app/home/new-design/home-side-nav/sidenav-items/sidenav-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
@@ -7,49 +7,8 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./sidenav-items.component.scss']
 })
 export class SidenavItemsComponent {
-@Input('category') category:string;
-
- Data = [
-     {
-       id: 1,
-       src:"../../../assets/images/trending/teapot/hummingbird-printed-t-shirt.jpg",
-       srcHover:"../../../assets/images/trending/teapot/hummingbird-printed-t-shirt3.jpg",
-       alt:'Side 1',
-       title:'Teapot',
-       productId:1,
-       old:"$43.90",new:'$21.90'
-     },
-     {
-       id: 2,
-       src:"../../../assets/images/trending/table/brown-bear-printed-sweater2.jpg",
-       srcHover:"../../../assets/images/trending/table/brown-bear-printed-sweater3.jpg",
-       alt:'Side 2',
-       title:'Miro Dinning Table',
-      productId:2,old:"$83.90",new:'$52.90',
-     },
-     {
-       id: 3,
-       src:"../../../assets/images/trending/TableLamp/the-best-is-yet-to-come-framed-poster2.jpg",
-       srcHover:"../../../assets/images/trending/TableLamp/the-best-is-yet-to-come-framed-poster.jpg",
-       alt:'Side 3',
-       title:'Janus Table Lamp',
-       productId:3,
-       old:"$73.90",new:'$41.90'
-     },
-     {
-       id: 4,
-       src:"../../../assets/images/trending/Discus/the-adventure-begins-framed-poster.jpg",
-       srcHover:"../../../assets/images/trending/Discus/the-adventure-begins-framed-poster2.jpg",
-       alt:'Side 4',
-       title:'Discus Floor and Table',
-       productId:4,
-        old:"$199.90",new:'$150.90'
-     },
-   ]
- DataLength:number=this.Data.length;
- DataSlideIndex=Math.ceil(((this.DataLength-1)/2));
- CarsouelOneData=this.Data.slice(0,this.DataSlideIndex);
- CarsouelTwoData=this.Data.slice(this.DataSlideIndex,this.DataLength);
+@Input('Data') Data:any;
+@ViewChild('doubleElements') doubleElements:any;
 customOptions: OwlOptions = {
     margin:16,
     loop: true,
@@ -62,30 +21,11 @@ customOptions: OwlOptions = {
     navText: ['&#8249', '&#8250;'],
     responsive: {
       0: {
-        items: 1 
+        items: 1
       },
-      576: {
+       355: {
         items: 2
-      }
-    },
-  }
-customOptionsTwo: OwlOptions = {
-    margin:16,
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: false,
-    dots: false,
-    nav:false,
-    navSpeed: 600,
-    navText: ['&#8249', '&#8250;'],
-    responsive: {
-      0: {
-        items: 1 
       },
-      576: {
-        items: 2
-      }
     },
   }
 
