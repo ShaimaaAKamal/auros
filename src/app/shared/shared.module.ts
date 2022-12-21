7a476acbdb3c580f2b11a73af70a7207fb8e2a57
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-21 10:14:07 +0200

    move components from app module to shared nodule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,16 @@ import { PageHeaderComponent } from './page-header/page-header.component';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [PageHeaderComponent,NavbarComponent,FooterComponent],
+  declarations: [PageHeaderComponent,NavbarComponent,FooterComponent,ProductCardComponent],
   imports: [
     CommonModule,
     SharedRoutingModule,FormsModule
   ],
-  exports:[PageHeaderComponent,FooterComponent,NavbarComponent]
+  exports:[PageHeaderComponent,FooterComponent,NavbarComponent,ProductCardComponent]
 })
 export class SharedModule { }
