66503ccb130cd1d6742cd6500e6963ebbe2ea0d4
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-23 15:30:48 +0200

    create service folder and move all services to it

---
+++ src/app/Services/CategoriesService/categories.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+ subCategories=[{name:'Metal',id:1,categoryId:1},{name:'Wooden',id:2,categoryId:1}]
+ mainCategories:any[]=[{title:'Kitchen',categories:[{name:'Dinner Table' ,id:1},{name:'Dinning Chairs',id:2},{name:'Side Board',id:3},{name:'Cermaic Tiles',id:4},{name:'Seat',id:5}]},
+            {title:'Living Room',categories:[{name:'Sofas',id:6},{name:'TV Shelfs',id:7},{name:'Coffee Tables',id:8},{name:'Bed Sheet',id:9},{name:'Cusions',id:10}]},
+            {title:'Decoration',categories:[{name:'Lamps',id:11},{name:'Lighting',id:12},{name:'Furnitures',id:13},{name:'Speakers',id:14},{name:'Stools',id:15}]}
+           ];
+  constructor() { }
+ getAllCategories(){
+  let categories:any[]=[];
+  this.mainCategories.forEach((mainCategory:any) => {
+       categories.push(...mainCategory.categories);
+  });
+  return categories;
+ }
+}
