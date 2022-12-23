72039992f5983f3cab43ad064e3b6ab7fd6e27e9
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-23 14:05:39 +0200

    implement new products side section

---
+++ src/app/shop/new-products/new-products.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ProductsService } from 'src/app/products.service';
+
+@Component({
+  selector: 'app-new-products',
+  templateUrl: './new-products.component.html',
+  styleUrls: ['./new-products.component.scss']
+})
+export class NewProductsComponent {
+ constructor(private __ProductsService:ProductsService){}
+ products:any[]=[];
+ ngOnInit(): void {
+   this.products=this.__ProductsService.getNewProducts();
+ }
+}
