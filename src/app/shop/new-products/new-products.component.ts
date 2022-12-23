cffe4b5d78588d465aa5e63c13e7204e7808ee35
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-23 15:32:47 +0200

    adjust component styling

--- src/app/shop/new-products/new-products.component.ts
+++ src/app/shop/new-products/new-products.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProductsService } from 'src/app/products.service';
+import { ProductsService } from 'src/app/Services/ProductService/products.service';
 
 @Component({
   selector: 'app-new-products',
