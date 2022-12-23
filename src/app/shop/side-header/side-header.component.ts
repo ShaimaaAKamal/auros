2b3952fbb7a73b85e085ca02dfe6ca7334e54d22
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-23 15:32:30 +0200

    implement side header html and css and basic logic and add it to the shop component

--- src/app/shop/side-header/side-header.component.ts
+++ src/app/shop/side-header/side-header.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CategoriesService } from 'src/app/Services/CategoriesService/categories.service';
 @Component({
   selector: 'app-side-header',
   templateUrl: './side-header.component.html',
   styleUrls: ['./side-header.component.scss']
 })
 export class SideHeaderComponent {
-
+categories:any[]=[];
+subCategories:any[]=[]
+constructor(private __CategoriesService:CategoriesService){}
+ngOnInit(): void {
+  this.categories=this.__CategoriesService.getAllCategories();
+  this.subCategories=this.__CategoriesService.subCategories;
+  this.categories.forEach(category =>{
+     category['noOfSubCategories']=this.subCategories.filter((sub:any) => sub.categoryId === category.id).length;
+  });
+}
+showSubCategories(event:any){
+  event.target.parentNode.parentNode.nextElementSibling.classList.toggle('d-none');
+}
 }
