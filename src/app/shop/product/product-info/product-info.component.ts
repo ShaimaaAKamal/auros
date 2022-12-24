1396952a8053377ccd8f89afe9a03cc9bdaa92f1
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-24 16:18:55 +0200

    add basic logic

--- src/app/shop/product/product-info/product-info.component.ts
+++ src/app/shop/product/product-info/product-info.component.ts
@@ -8,5 +8,18 @@ import { Component, Input } from '@angular/core';
 export class ProductInfoComponent {
 @Input() product:any;
 @Input() writeReview:any;
+quantity:number=1;
 productImages=[1,2,3,4,5]
+qunatityIncreament(){
+     this.quantity=this.quantity+1;
+}
+qunatitydecreament(){
+  this.quantity=this.quantity <=0 ? 0 : this.quantity-1;
+}
+   addToCart(){
+  }
+  addToWishlist(event:any,id:number){
+  }
+  addToCompare(event:any,id:number){
+  }
 }
