e0af31af7d4622361d17177dc532509f9389fb5e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-24 13:10:55 +0200

    init product component

--- src/app/shop/product/product.component.ts
+++ src/app/shop/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../Services/ProductService/products.service';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-
+  constructor(private __ActivatedRoute:ActivatedRoute,private __ProductsService:ProductsService){}
+  paths=[{title:'Home',url:''}];
+  product:any;
+  ngOnInit(): void {
+    const productId=this.__ActivatedRoute.snapshot.params['id'];
+    const product=this.__ProductsService.getProductById(productId);
+    this.product=product;
+    this.paths.push({title:product.category, url:`/shop/categories/${product.categoryId}/${product.category.split(' ').join('_')}`});
+    this.paths.push({title:product.title,url:`/shop/product/${product.id}`})
+  }
 }
