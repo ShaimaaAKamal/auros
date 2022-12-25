145e25ad3920ffa4f17fd4f86af41ab598c22a42
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-25 14:40:06 +0200

    remove subcategories from the service and call maincategory services

--- src/app/Services/CategoriesService/categories.service.ts
+++ src/app/Services/CategoriesService/categories.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Category } from 'src/app/Interfaces/category';
+import { ShopInterface } from 'src/app/Interfaces/shop-interface';
+import { MainCategoryServiceService } from '../MainCategoryService/main-category-service.service';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
- categories:any[]=[];
- subCategories=[{name:'Metal',id:1,categoryId:1},{name:'Wooden',id:2,categoryId:1}]
- mainCategories:any[]=[{title:'Kitchen',categories:[{name:'Dinner Table' ,id:1},{name:'Dinning Chairs',id:2},{name:'Side Board',id:3},{name:'Cermaic Tiles',id:4},{name:'Seat',id:5}]},
-            {title:'Living Room',categories:[{name:'Sofas',id:6},{name:'TV Shelfs',id:7},{name:'Coffee Tables',id:8},{name:'Bed Sheet',id:9},{name:'Cusions',id:10}]},
-            {title:'Decoration',categories:[{name:'Lamps',id:11},{name:'Lighting',id:12},{name:'Furnitures',id:13},{name:'Speakers',id:14},{name:'Stools',id:15}]}
-           ];
-  constructor() {  this.categories=this.getAllCategories();  }
+ categories:Category[]=[];
+ mainCategories:ShopInterface[]=this.__MainCategoryServiceService.getMainCategories();
+constructor(private __MainCategoryServiceService:MainCategoryServiceService ) {  this.categories=this.getAllCategories();  }
 
  getAllCategories(){
-  const categories:any[]=[]
+  const categories:Category[]=[]
   this.mainCategories.forEach((mainCategory:any) => {
     categories.push(...mainCategory.categories);
   });
@@ -22,4 +23,5 @@ export class CategoriesService {
  getCategoryById(id:string){
     return this.categories.find(category => category.id===parseInt(id));
    }
+  
 }
