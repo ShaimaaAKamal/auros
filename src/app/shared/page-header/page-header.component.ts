9dc8c628d5092d231fbe4dc15d14f4e48cbdbb3b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-25 15:48:17 +0200

    change any type with path

--- src/app/shared/page-header/page-header.component.ts
+++ src/app/shared/page-header/page-header.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { Path } from 'src/app/Interfaces/path';
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss']
 })
 export class PageHeaderComponent {
-@Input('paths') paths:any;
+@Input('paths') paths:Path[];
 @Input('mainTitle') mainTitle:string='';
  }
