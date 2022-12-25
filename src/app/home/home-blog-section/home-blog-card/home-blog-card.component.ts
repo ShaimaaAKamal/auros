06c7e58a50465a8d49d98a641b1b1d9f4e831c18
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-25 12:19:26 +0200

    convert blog type to blog instead of any

--- src/app/home/home-blog-section/home-blog-card/home-blog-card.component.ts
+++ src/app/home/home-blog-section/home-blog-card/home-blog-card.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
-
+import { Blog } from 'src/app/Interfaces/blog';
 @Component({
   selector: 'app-home-blog-card',
   templateUrl: './home-blog-card.component.html',
   styleUrls: ['./home-blog-card.component.scss']
 })
 export class HomeBlogCardComponent {
-@Input('blog') blog:any;
+@Input('blog') blog:Blog;
 }
