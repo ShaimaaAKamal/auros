da4e7f140063bfab92bdb922dd47f4d627b9b823
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-27 10:55:04 +0200

    save reviews to local storage

--- src/app/Services/ReviewService/review.service.ts
+++ src/app/Services/ReviewService/review.service.ts
@@ -6,18 +6,23 @@ import { Review } from '../../Interfaces/review';
 })
 
 export class ReviewService {
-  reviews:Review[]=[
-    { id:1,rate:3,title:'review 1',Message:'this is the first review message',productId:1,userId:1,date:new Date()},
-    { id:2,rate:4,title:'review 2',Message:'this is the an review message',productId:1,userId:2,date:new Date()},
-    { id:3,rate:3,title:'review 3',Message:'this is the second review message',productId:2,userId:3,date:new Date()},
-    { id:4,rate:2,title:'review 4',Message:'this is the third review message',productId:2,userId:1,date:new Date()},
-    { id:5,rate:5,title:'review 5',Message:'this is the forth review message',productId:1,userId:2,date:new Date()},
-    { id:6,rate:1,title:'review 6',Message:'this is the fifth review message',productId:3,userId:1,date:new Date()},
-    { id:7,rate:0,title:'review 7',Message:'this is the sixth review message',productId:3,userId:2,date:new Date()},
-    { id:8,rate:2,title:'review 8',Message:'this is the seneth review message',productId:2,userId:3,date:new Date()},
+  reviews:Review[]=[];
+  staticReviews:Review[]=[
+    { id:1,rate:3,title:'review 1',Message:'this is the first review message',productId:1,userId:1,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
+    { id:2,rate:4,title:'review 2',Message:'this is the an review message',productId:1,userId:2,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
+    { id:3,rate:3,title:'review 3',Message:'this is the second review message',productId:2,userId:3,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
+    { id:4,rate:2,title:'review 4',Message:'this is the third review message',productId:2,userId:1,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
+    { id:5,rate:5,title:'review 5',Message:'this is the forth review message',productId:1,userId:2,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
+    { id:6,rate:1,title:'review 6',Message:'this is the fifth review message',productId:3,userId:1,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
+    { id:7,rate:0,title:'review 7',Message:'this is the sixth review message',productId:3,userId:2,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
+    { id:8,rate:2,title:'review 8',Message:'this is the seneth review message',productId:2,userId:3,date:new Date().toLocaleDateString(),useful:0,unuseful:0},
 
   ]
-  constructor() { }
+  constructor() { 
+    const reviews=localStorage.getItem('reviews');
+    this.reviews=reviews ? JSON.parse(reviews) : this.staticReviews;
+    localStorage.setItem('reviews',JSON.stringify(this.reviews));
+  }
 
   getReviewByProductID(id:number){
     return this.reviews.filter((review:Review) => review.productId === id);
@@ -36,11 +41,22 @@ export class ReviewService {
   }
   addNewReview(newReview:Review){
         this.reviews.push(newReview);
+        localStorage.setItem('reviews',JSON.stringify(this.reviews));
   }
   deleteReview(reviewId:number){
     this.reviews.splice(reviewId,1);
+    localStorage.setItem('reviews',JSON.stringify(this.reviews));
   }
   getReviewById(reviewId:number){
          return this.reviews.find((review:Review) => review.id === reviewId);
+  }
+  updateReviewById(reviewId:number,key:string,newValue:any):Review[]{
+         let reviewItem:any=this.reviews.find((review:Review) => review.id === reviewId);
+         if(reviewItem) {
+          reviewItem[key]=newValue;
+          this.reviews.splice(reviewId,1,reviewItem);
+          localStorage.setItem('reviews',JSON.stringify(this.reviews));
+         }
+          return this.reviews;
   }
 }
