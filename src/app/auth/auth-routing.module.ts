0b85b11c84c1dab1cd3ef52f9f08151892670b4f
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-27 10:56:45 +0200

    add forgetPassword route

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 const routes: Routes = [
   {path:'',component:ProfileComponent,title:'Profile'},
   {path:'login',component:LoginComponent,title:'Login'},
   {path:'register',component:RegisterComponent,title:'Register'},
+  {path:'forgetPassword',component:ForgetPasswordComponent,title:'Register'},
+
 ];
 
 @NgModule({
