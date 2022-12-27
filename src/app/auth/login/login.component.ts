ed708cf472cb7ecf8f58a87aa4bbeda5ec07726d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-27 10:52:35 +0200

    implement login component and init login logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Path } from 'src/app/Interfaces/path';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  paths:Path[]=[{title:'Home',url:""},{title:'Login'}];
+  visible:boolean=false;
 
+ showPassword(){
+
+ }
+hidePassword(){
+  
+ }
 }
