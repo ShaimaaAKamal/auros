5601dcdec30b69b8af8d2ebf6457037a71fbc811
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-27 10:53:17 +0200

    implement regiseter component and init register logic

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Path } from 'src/app/Interfaces/path';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  paths:Path[]=[{title:'Home',url:""},{title:'Register'}];
+    visible:boolean=false;
+
+ showPassword(){
+
+ }
+hidePassword(){
+  
+ }
 
 }
