e32781e00fa4308dfcc3ab217f0883abb71c8447
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-27 10:51:48 +0200

    generate carsouel component and add it to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { CommonModule } from '@angular/common';
 import { PageHeaderComponent } from './page-header/page-header.component';
 import { SharedRoutingModule } from './shared-routing.module';
@@ -8,14 +9,13 @@ import { ProductCardComponent } from './product-card/product-card.component';
 import { FormsModule } from '@angular/forms';
 import { AddtowishlistComponent } from './addtowishlist/addtowishlist.component';
 import { AddtocomparelistComponent } from './addtocomparelist/addtocomparelist.component';
-
-
+import { CarsouelComponentComponent } from './carsouel-component/carsouel-component.component';
 @NgModule({
-  declarations: [PageHeaderComponent,NavbarComponent,FooterComponent,ProductCardComponent, AddtowishlistComponent, AddtocomparelistComponent],
+  declarations: [PageHeaderComponent,NavbarComponent,FooterComponent,ProductCardComponent, AddtowishlistComponent, AddtocomparelistComponent, CarsouelComponentComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule,FormsModule
+    SharedRoutingModule,FormsModule,CarouselModule,
   ],
-  exports:[PageHeaderComponent,FooterComponent,NavbarComponent,ProductCardComponent]
+  exports:[PageHeaderComponent,FooterComponent,NavbarComponent,ProductCardComponent,CarsouelComponentComponent]
 })
 export class SharedModule { }
