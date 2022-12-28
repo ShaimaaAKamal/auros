12acae26f03eebfa49af7c49bf9b34bc5a7d1533
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-28 11:28:23 +0200

    implement contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
 import { Path } from '../Interfaces/path';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { MailService } from '../Services/Email/mail.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  paths:Path[]=[{title:'Home',url:''},{title:'Contact us'}]
+  paths:Path[]=[{title:'Home',url:''},{title:'Contact us'}];
+   states = [
+    {name: 'Customer Service'},
+    {name: 'WebMaster'},
+  ];
+   user=localStorage.getItem('user');
+   userEmail=this.user?JSON.parse(this.user).email:'your@example.com';
+   displayError:boolean=false;
+   notSent:boolean=false;
+   sent:boolean=false;
+  contactForm:FormGroup=new FormGroup({
+    subject: new FormControl(this.states[0]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    attachment: new FormControl(''),
+    message: new FormControl('',[Validators.required]),
+    check: new FormControl(''),
 
+  });
+  constructor(private __MailService:MailService){}
+  submitContactForm(){
+    if(this.contactForm.invalid) this.displayError=true;
+    else{
+      const formData:any={}
+      formData['email']=this.contactForm.value.email;
+      formData['message']=this.contactForm.value.message;
+      formData['_subject']=this.contactForm.value.subject.name;
+      this.__MailService.SendEmail(formData).subscribe({
+          next:(response:any) => {
+            this.notSent=false;
+            this.sent=true;
+            this.contactForm.reset();
+          },
+          error: error => {
+            console.log({ error });
+            this.notSent=true;
+            this.sent=false;
+    }     
+        })
+    }
+  }
 }
