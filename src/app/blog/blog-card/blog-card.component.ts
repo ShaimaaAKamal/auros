b6d7994a19af1d01edbe83709f148917f38271b8
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-28 13:57:29 +0200

    implement blog card

---
+++ src/app/blog/blog-card/blog-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { Blog } from 'src/app/Interfaces/blog';
+@Component({
+  selector: 'app-blog-card',
+  templateUrl: './blog-card.component.html',
+  styleUrls: ['./blog-card.component.scss']
+})
+export class BlogCardComponent {
+  @Input() blog:Blog;
+}
