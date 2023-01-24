f86dc026d8c33ba6ebd28b10fb84cebbede41b3a
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2023-01-24 22:50:54 +0200

    update blog Interface

--- src/app/Interfaces/blog.ts
+++ src/app/Interfaces/blog.ts
@@ -1,12 +1,18 @@
+import { Comment } from "./comment";
 export interface Blog {
     id:number,
     title:string,
     image:string,
     description:string,
+    secondDesc?:string,
+    thirdDesc?:string,
     category:string,
     date:string,
     noOfLikes:number,
     author:string,
     noOfComments:number,
     authorId?:number,
+    tag?:string,
+    comments?:Comment[];
+    src?:string,
 }
