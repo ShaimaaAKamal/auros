334cce0aca7ea7e58517b8139926f17e5061c16f
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2023-01-24 22:54:48 +0200

    add auth Guards to define which pages is accessed as a guest and as a authenticated user

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,14 +4,16 @@ import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { AuthGuard } from '../Guards/auth.guard';
+import { GuestGuard } from '../Guards/guestGuard/authGuard/guest.guard';
 
 
 const routes: Routes = [
-  {path:'',component:ProfileComponent,title:'Profile'},
-  {path:'login',component:LoginComponent,title:'Login'},
-  {path:'register',component:RegisterComponent,title:'Register'},
-  {path:'forgetPassword',component:ForgetPasswordComponent,title:'Register'},
-
+  // {path:'',component:ProfileComponent,title:'Profile'},
+  {path:'login',canActivate:[GuestGuard],component:LoginComponent,title:'Login'},
+  {path:'register',canActivate:[GuestGuard],component:RegisterComponent,title:'Register'},
+  {path:'forgetPassword',component:ForgetPasswordComponent,title:'Forget Password'},
+  {path:'profile/:userId',canActivate:[AuthGuard],component:ProfileComponent,title:'Profile'},
 ];
 
 @NgModule({
