146fcd3cf6f36c41433bc0bd6ce98cec1591ff7b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2023-01-24 22:56:09 +0200

    remove social  media provider and move them to app component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
@@ -8,7 +7,8 @@ import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+// import { GoogleLoginProvider, SocialLoginModule , SocialAuthServiceConfig} from '@abacritt/angularx-social-login';
+// import { FacebookLoginProvider } from '@abacritt/angularx-social-login';
 @NgModule({
   declarations: [
     ProfileComponent,
@@ -16,11 +16,36 @@ import { ReactiveFormsModule } from '@angular/forms';
     LoginComponent,
     ForgetPasswordComponent
   ],
+  //    providers: [
+      
+  //   {
+  //     provide: 'SocialAuthServiceConfig',
+  //     useValue: {
+  //       autoLogin: false,
+  //       providers: [
+  //         {
+  //           id: GoogleLoginProvider.PROVIDER_ID,
+  //           provider: new GoogleLoginProvider(
+  //             '833486081205-skkvhahhdmmrhc2mm9h809sd10eul7hn.apps.googleusercontent.com'
+  //           )
+  //         },
+  //         {
+  //           id: FacebookLoginProvider.PROVIDER_ID,
+  //           provider: new FacebookLoginProvider('2882402905230036')
+  //         }
+  //       ],
+  //       onError: (err) => {
+  //         console.error(err);
+  //       }
+  //     } as SocialAuthServiceConfig,
+  //   }
+  // ],
   imports: [
     CommonModule,
     AuthRoutingModule,
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // SocialLoginModule,
   ]
 })
 export class AuthModule { }
